bc285f286020dcfe1f5f969626b4d5039a290447
Ferdinand Mudjialim <ferdinandmudjialim@gmail.com>
2019-01-23 16:22:07 -0600

    Minor update with a bugfix and some code cleanup. Book entries in input.txt can now be written in English.

--- BibleScraper.py
+++ BibleScraper.py
@@ -5,26 +5,44 @@
 import requests, bs4
 
 
-def printVerses(bookName, chapterNumber, verseNumbers):
+def printVerses(book, chapterNumber, verseNumbers):
 
-    bookList = ['kejadian', 'keluaran', 'imamat', 'bilangan', 'ulangan',
-                'yosua', 'hakim-hakim', 'rut', '1samuel', '2samuel',
-                '1raja-raja', '2raja-raja', '1tawarikh', '2tawarikh',
-                'ezra', 'nehemia', 'ester', 'ayub', 'mazmur', 'amsal',
-                'pengkhotbah', 'kidungagung', 'yesaya', 'yeremia', 'ratapan',
-                'yehezkiel', 'daniel', 'hosea', 'yoel', 'amos', 'obaja',
-                'yunus', 'mikha', 'nahum', 'habakuk', 'zefanya', 'hagai',
-                'zakharia', 'maleakhi', 'matius', 'markus', 'lukas', 'yohanes',
-                'kisahpararasul', 'roma', '1korintus', '2korintus',
-                'galatia', 'efesus', 'filipi', 'kolose', '1tesalonika',
-                '2tesalonika', '1timotius', '2timotius', 'titus', 'filemon',
-                'ibrani', 'yakobus', '1petrus', '2petrus', '1yohanes',
-                '2yohanes', '3yohanes', 'yudas', 'wahyu']
-    bookNumbers = ['0' + str(n) for n in range(1, 10)] \
+    bookIndList = ['kejadian', 'keluaran', 'imamat', 'bilangan', 'ulangan',
+                    'yosua', 'hakim-hakim', 'rut', '1samuel', '2samuel',
+                    '1raja-raja', '2raja-raja', '1tawarikh', '2tawarikh',
+                    'ezra', 'nehemia', 'ester', 'ayub', 'mazmur', 'amsal',
+                    'pengkhotbah', 'kidungagung', 'yesaya', 'yeremia', 'ratapan',
+                    'yehezkiel', 'daniel', 'hosea', 'yoel', 'amos', 'obaja',
+                    'yunus', 'mikha', 'nahum', 'habakuk', 'zefanya', 'hagai',
+                    'zakharia', 'maleakhi', 'matius', 'markus', 'lukas', 'yohanes',
+                    'kisahpararasul', 'roma', '1korintus', '2korintus',
+                    'galatia', 'efesus', 'filipi', 'kolose', '1tesalonika',
+                    '2tesalonika', '1timotius', '2timotius', 'titus', 'filemon',
+                    'ibrani', 'yakobus', '1petrus', '2petrus', '1yohanes',
+                    '2yohanes', '3yohanes', 'yudas', 'wahyu']
+    bookEngList = ['genesis', 'exodus', 'leviticus', 'numbers', 'deuteronomy',
+                    'joshua', 'judges', 'ruth', '1samuel', '2samuel', '1kings',
+                    '2kings', '1chronicles', '2chronicles', 'ezra', 'nehemiah',
+                    'esther', 'job', 'psalm', 'proverbs', 'ecclesiastes', 
+                    'songofsongs', 'isaiah', 'jeremiah', 'lamentations', 
+                    'ezekiel', 'daniel', 'hosea', 'joel', 'amos', 'obadiah', 
+                    'jonah', 'micah', 'nahum', 'habakkuk', 'zephaniah', 
+                    'haggai', 'zechariah', 'malachi', 'matthew', 'mark', 
+                    'luke', 'john', 'acts', 'romans', '1corinthians', 
+                    '2corinthians', 'galatians', 'ephesians', 'philippians', 
+                    'colossians', '1thessalonians', '2thessalonians', 
+                    '1timothy', '2timothy', 'titus', 'philemon', 'hebrews', 
+                    'james', '1peter', '2peter', '1john', '2john', '3john', 
+                    'jude', 'revelation']
+
+    assert len(bookIndList) == len(bookEngList)  # make sure number of books =
+
+    bookEncodings = ['0' + str(n) for n in range(1, 10)] \
                   + [str(m) for m in range(10, 67)]
-    bookDict = dict(zip(bookList, bookNumbers))
+    bookIndDict = dict(zip(bookIndList, bookEncodings))
+    bookEngDict = dict(zip(bookEngList, bookEncodings))
 
-    book = str(bookName.strip().lower())  # 2 digits format ex: 01
+    bookName = str(book.strip().lower())  # 2 digits format ex: 01
     chapter = str(chapterNumber.strip())  # 3 digits format ex: 001
     if len(chapter) <= 2:  # correct formatting for chapter numbers
         if len(chapter) == 1:
@@ -33,13 +51,24 @@ def printVerses(bookName, chapterNumber, verseNumbers):
             chapter = '0' + chapter
     verseInput = verseNumbers.strip()
 
+    # If book name in Indonesian
+    if bookName in bookIndList:
+        bookNumber = bookIndDict[bookName]
+    # If book name in English
+    elif bookName in bookEngList:
+        bookNumber = bookEngDict[bookName]
+    else: 
+        raise Exception(book)
 
-    res = requests.get('http://www.bibledbdata.org/onlinebibles/indonesian_tb/'
-                       + str(bookDict[book])
-                       + '_'
-                       + str(chapter)
-                       + '.htm')
-    res.raise_for_status()
+    try: 
+        res = requests.get('http://www.bibledbdata.org/onlinebibles/indonesian_tb/'
+                           + str(bookNumber)  # book as number in URL string
+                           + '_'
+                           + str(chapter)  # chapter as number in URL string
+                           + '.htm')
+        res.raise_for_status()
+    except Exception as e: 
+        print("Error in book argument", e.args[0])
 
     bibleSoup = bs4.BeautifulSoup(res.text, "html.parser")
     bibleSoup = bibleSoup.select('blockquote')
@@ -70,21 +99,28 @@ def printVerses(bookName, chapterNumber, verseNumbers):
             verseTo = int(parsedVerseNumbers[1])
 
     temp = ''
-    temp += '\n' + book.title() + ' ' + chapter.lstrip('0') + '\n'
+    temp += '\n' + bookName.title() + ' ' + chapter.lstrip('0') + '\n'
     for i in range(verseFrom, verseTo):
         temp += verseList[i] + '\n'
     return temp
+    
+def main(): 
+    with open('input.txt') as fp:  # read from input.txt
+        with open('output.txt', 'w') as out:  # write to output.txt
+            line = fp.readline()
+            while line != '':
+                lineParseList = line.split()
+                # If there are additional blank lines on the bottom
+                if lineParseList == []:
+                    break
+                # Ignoring comment lines
+                if lineParseList[0] == '#':
+                    line = fp.readline()
+                else:
+                    out.write(printVerses(lineParseList[0], lineParseList[1],
+                                          lineParseList[2]))
+                    line = fp.readline()
 
+if __name__ == '__main__':
+    main()
 
-with open('input.txt') as fp:
-    with open('output.txt', 'w') as out:
-        line = fp.readline()
-        while line != '':
-            lineParseList = line.split()
-
-            if lineParseList[0] == '#':
-                line = fp.readline()
-            else:
-                out.write(printVerses(lineParseList[0], lineParseList[1],
-                                      lineParseList[2]))
-                line = fp.readline()
